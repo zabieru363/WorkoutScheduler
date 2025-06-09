e6d94948302cd7534fe585728edb2889d766e37a
zabieru363 <zabierujlc@gmail.com>
2025-06-09 18:13:24 +0200

    Project created. Database and application properties configured

---
+++ backend/workout-scheduler-app/src/main/java/com/workout/scheduler/app/workout_scheduler_app/WorkoutSchedulerAppApplication.java
@@ -0,0 +1,13 @@
+package com.workout.scheduler.app.workout_scheduler_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutSchedulerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutSchedulerAppApplication.class, args);
+	}
+
+}
